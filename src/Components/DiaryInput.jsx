2dac00bf259226455d9d70587903ba0b44045716
gull2365 <jych2365@gmail.com>
2023-12-26 15:33:21 +0900

    feat: 테스팅 및 코드 정리

--- src/Components/DiaryInput.jsx
+++ src/Components/DiaryInput.jsx
@@ -1,28 +1,55 @@
-import { Input, Button } from "antd";
+import { Input, Button, message } from "antd";
 import { useState } from "react";
 const { TextArea } = Input;
+import { Title } from "./CommonStyles";
+import styled from "styled-components";
 
-export const DiaryInput = ({ isLoading, onSubmit }) => {
+export const DiaryInput = ({ isLoading, onSubmit, messageApi }) => {
   const [userInput, setUserInput] = useState(""); // 사용자 입력값
+  // const [messageApi, contextHolder] = message.useMessage();
 
   const handleUserInput = (e) => {
     setUserInput(e.target.value);
   };
 
   const handleClick = () => {
+    if (!userInput) {
+      messageApi.open({
+        type: "error",
+        content: "일과를 작성해주세요.",
+      });
+      return;
+    }
+    messageApi.open({
+      type: "success",
+      content: "생성 요청 완료!",
+    });
+
     onSubmit(userInput);
+    setUserInput("");
   };
 
   return (
     <div>
+      <Title>오늘의 일:</Title>
       <TextArea
         value={userInput}
         onChange={handleUserInput}
         placeholder="오늘 일어난 일을 간단히 작성해주세요."
+        style={{ height: "200px" }}
       />
-      <Button loading={isLoading} onClick={handleClick}>
-        GPT 회고록을 작성해줘!
-      </Button>
+      <ButtonContainer>
+        <Button loading={isLoading} onClick={handleClick}>
+          GPT 회고록을 작성해줘!
+        </Button>
+      </ButtonContainer>
     </div>
   );
 };
+
+const ButtonContainer = styled.div`
+  margin: 20px;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: flex-end;
+`;
